cfbb8119abeba3324c57a6ed8c8128dedbe6a3ea
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-04-25 18:15:35 +0530

    Add files via upload

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
+
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const cat = useLocation().search
+
+  useEffect(()=>{
+    const fetchData = async() => {
+      try {
+        const res = await axios.get(`http://localhost:4000/api/posts${cat}`)
+        setPosts(res.data)
+      } catch (error) {
+        console.log(`Home.jsx me error hai ${error}`);
+      }
+    }
+    fetchData()
+  } , [cat]);
+  return (
+    <>
+      <Navbar />
+      <div className="home">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="img">
+                <img src={post.img} alt={post.title} />
+              </div>
+              <div className="content">
+                <Link className="link" to={`/post/${post.id}`}>
+                  <h1>{post.title}</h1>
+                </Link>
+                <p>{post.desc}</p>
+                <Link className="link" to={`/post/${post.id}`}>
+                <button>Read More</button>
+                </Link>
+
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
