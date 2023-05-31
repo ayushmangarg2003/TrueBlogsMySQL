bb01a0c3f56a12bedbc230b56229060f3feb4073
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-05-31 11:52:36 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,60 +1,59 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Loader from "../components/Loader"
+import Loader from "../components/Loader";
 import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
-
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const cat = useLocation().search
+  const cat = useLocation().search;
 
-  useEffect(()=>{
-    const fetchData = async() => {
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        const res = await axios.get(`https://blogs-backend-l7v4.onrender.com/api/posts${cat}`)
-        setPosts(res.data)
-        setLoading(false)
-
+        const res = await axios.get(
+          `https://blogs-backend-l7v4.onrender.com/api/posts${cat}`
+        );
+        setPosts(res.data);
+        setLoading(false);
       } catch (error) {
         console.log(`Home.jsx me error hai ${error}`);
-        setLoading(false)
+        setLoading(false);
       }
-    }
-    fetchData()
-  } , [cat]);
-  return (
-    <>
-      <Navbar />
-    {loading ? (<Loader/> : (
-      <div className="home">
-        <div className="posts">
-          {posts.map((post) => (
-            <div className="post" key={post.id}>
-              <div className="img">
-                <img src={post.img} alt={post.title} />
+    };
+    fetchData();
+  }, [cat]);
+  return <>
+        <Navbar />
+  {loading ? (<Loader />): (
+      <>
+        <div className="home">
+          <div className="posts">
+            {posts.map((post) => (
+              <div className="post" key={post.id}>
+                <div className="img">
+                  <img src={post.img} alt={post.title} />
+                </div>
+                <div className="content">
+                  <Link className="link" to={`/post/${post.id}`}>
+                    <h1>{post.title}</h1>
+                  </Link>
+                  <p>{post.descr}</p>
+                  <Link className="link" to={`/post/${post.id}`}>
+                    <button>Read More</button>
+                  </Link>
+                </div>
               </div>
-              <div className="content">
-                <Link className="link" to={`/post/${post.id}`}>
-                  <h1>{post.title}</h1>
-                </Link>
-                <p>{post.descr}</p>
-                <Link className="link" to={`/post/${post.id}`}>
-                <button>Read More</button>
-                </Link>
-
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-      )}
-      <Footer />
-    </>
-  );
-};
-
+        <Footer />
+      </>
+      )
+    }
+  </>
+}
 export default Home;
